f06c0f2847b48776d6f5d80722f9af60910aecdb
slkennedy <slkennedy@gmail.com>
2014-09-18 09:15:53 -0400

    Adds JS and Underscore template - flickr API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('The Iron Yard Rocks');
+function reusableFunction(container, templateID, model) {
+    var templateFunction = _.template($('#' + templateID).text());
+    var renderTemplate = templateFunction(model);
+    $(container).append(renderTemplate);
+}
+
+$.ajax('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=cda189c50310b7c6d848431b48d44b2d&format=json&tags=bike,bicycles,bikes,bicycle', {
+	type: 'GET',
+	datatype: 'json'
+}).done(function(images){
+	_.each(function(image){
+		console.log(image)
+		reusableFunction('.images', 'images-template', image);
+	});
+});
