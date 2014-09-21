334a6fae15cb9907db95c3cdbc08ab81fc1b6b49
slkennedy <slkennedy@gmail.com>
2014-09-21 14:06:32 -0400

    Adds map marker and styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,28 +15,3 @@ $.ajax( {
 		reusableFunction('.images', 'images-template', image);
 	});
 });
-
-// var googUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBw4K03wb2-iZFymKEeU3hx9dBgp7iJGXk">
-
-// $.ajax({
-// 	url: googUrl,
-// 	type: 'GET',
-// 	dataType: 'json',
-// 	center: {lat: 34.846791, lng: -82.402418},
-//     zoom: 11
-// })
-// .done(function(maps){
-// 	var map = new google.maps.Map(document.getElementById('mapped'),
-//  	maps);
-// })
-// .done(function(loading) {
-//   	google.maps.event.addDomListener('mapped', 'load', maps);
-//   });
-
-
-// // function mapping () {
-// //   var mapOptions = {
-// //     center: new google.maps.LatLng (34.846791, -82.402418),
-// //     zoom: 11
-// //   };
-
